f1ea905629093f58988084b20f2c0495a848e659
topone233 <55429221+topone233@users.noreply.github.com>
2021-01-25 17:01:08 +0800

    Update request.py

--- request.py
+++ request.py
@@ -6,7 +6,7 @@ from email.mime.text import MIMEText
 
 base_url = 'https://leetcode-cn.com'
 # 获取今日每日一题的题名(英文)
-response = requests.post(base_url + "/topone233", json={
+response = requests.post(base_url + "/graphql", json={
     "operationName": "questionOfToday",
     "variables": {},
     "query": "query questionOfToday { todayRecord {   question {     questionFrontendId     questionTitleSlug     __typename   }   lastSubmission {     id     __typename   }   date   userStatus   __typename }}"
@@ -15,7 +15,7 @@ leetcodeTitle = json.loads(response.text).get('data').get('todayRecord')[0].get(
 
 # 获取今日每日一题的所有信息
 url = base_url + "/problems/" + leetcodeTitle
-response = requests.post(base_url + "/topone233",
+response = requests.post(base_url + "/graphql",
                          json={"operationName": "questionData", "variables": {"titleSlug": leetcodeTitle},
                                "query": "query questionData($titleSlug: String!) {  question(titleSlug: $titleSlug) {    questionId    questionFrontendId    boundTopicId    title    titleSlug    content    translatedTitle    translatedContent    isPaidOnly    difficulty    likes    dislikes    isLiked    similarQuestions    contributors {      username      profileUrl      avatarUrl      __typename    }    langToValidPlayground    topicTags {      name      slug      translatedName      __typename    }    companyTagStats    codeSnippets {      lang      langSlug      code      __typename    }    stats    hints    solution {      id      canSeeDetail      __typename    }    status    sampleTestCase    metaData    judgerAvailable    judgeType    mysqlSchemas    enableRunCode    envInfo    book {      id      bookName      pressName      source      shortDescription      fullDescription      bookImgUrl      pressImgUrl      productUrl      __typename    }    isSubscribed    isDailyQuestion    dailyRecordStatus    editorType    ugcQuestionId    style    __typename  }}"})
 # 转化成json格式
