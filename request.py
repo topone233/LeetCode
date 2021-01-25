103f54d81d71f9f45fb927a0b28d58bb833d578c
topone233 <55429221+topone233@users.noreply.github.com>
2021-01-25 20:19:25 +0800

    Update request.py

--- request.py
+++ request.py
@@ -11,15 +11,7 @@ base_url = 'https://leetcode-cn.com'
 response = requests.post(base_url + "/graphql", json={
     "operationName": "questionOfToday",
     "variables": {},
-    "query": "query questionOfToday { 
-        todayRecord {  
-            question { questionFrontendId     questionTitleSlug     __typename   }   
-            lastSubmission {     id     __typename   }   
-            date   
-            userStatus  
-            __typename 
-         }
-    }"
+    "query": "query questionOfToday { todayRecord {   question {     questionFrontendId     questionTitleSlug     __typename   }   lastSubmission {     id     __typename   }   date   userStatus   __typename }}"
 })
 leetcodeTitle = json.loads(response.text).get('data').get('todayRecord')[0].get("question").get('questionTitleSlug')
 
@@ -28,23 +20,7 @@ url = base_url + "/problems/" + leetcodeTitle
 response = requests.post(base_url + "/graphql", json={
     "operationName": "questionData", 
     "variables": {"titleSlug": leetcodeTitle},
-    "query": "query questionData($titleSlug: String!) { 
-        question(titleSlug: $titleSlug) {    
-            questionId    questionFrontendId    boundTopicId    title    titleSlug   
-            content    translatedTitle    translatedContent    isPaidOnly    difficulty   
-            likes    dislikes    isLiked    similarQuestions    contributors {  username      profileUrl      avatarUrl      __typename    } 
-            langToValidPlayground    topicTags {      name      slug      translatedName      __typename    }  
-            companyTagStats    codeSnippets {      lang      langSlug      code      __typename    }   
-            stats    hints    solution {      id      canSeeDetail      __typename    }   
-            status    sampleTestCase    metaData    judgerAvailable    judgeType    mysqlSchemas    enableRunCode    envInfo  book 
-            {      
-                id      bookName      pressName      source      shortDescription      fullDescription      
-                  bookImgUrl      pressImgUrl      productUrl      __typename   
-            }    
-            isSubscribed    isDailyQuestion    dailyRecordStatus    editorType    ugcQuestionId    style    __typename  
-        }
-    }"
-})
+    "query": "query questionData($titleSlug: String!) {  question(titleSlug: $titleSlug) {    questionId    questionFrontendId    boundTopicId    title    titleSlug    content    translatedTitle    translatedContent    isPaidOnly    difficulty    likes    dislikes    isLiked    similarQuestions    contributors {      username      profileUrl      avatarUrl      __typename    }    langToValidPlayground    topicTags {      name      slug      translatedName      __typename    }    companyTagStats    codeSnippets {      lang      langSlug      code      __typename    }    stats    hints    solution {      id      canSeeDetail      __typename    }    status    sampleTestCase    metaData    judgerAvailable    judgeType    mysqlSchemas    enableRunCode    envInfo    book {      id      bookName      pressName      source      shortDescription      fullDescription      bookImgUrl      pressImgUrl      productUrl      __typename    }    isSubscribed    isDailyQuestion    dailyRecordStatus    editorType    ugcQuestionId    style    __typename  }}"})
 # 转化成json格式
 jsonText = json.loads(response.text).get('data').get("question")
 # 题目题号
